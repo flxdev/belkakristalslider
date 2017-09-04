bfae8fad22e4c4bcfb1c8b28a7b3d1bdde8ac9ee
kotvasili <kot_vasili@mail.ru>
2017-09-04 15:42:17 +0300

    slider

---
+++ src/js/common.js
@@ -0,0 +1,26 @@
+$(window).on('load', function() {
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+if ($('.js-jcarousel-catalog2').length) {
+		$('.js-jcarousel-catalog2').carouFredSel({ 
+			auto: false,
+			width: "100%", 
+			scroll: {
+				items: 1,
+				pauseOnHover: true
+			},
+			items: {
+				visible: 3
+			},	
+			prev: '.jcarousel4-catalog-control-prev',
+			next: '.jcarousel4-catalog-control-next',  
+			swipe: {
+				onMouse: false,
+				onTouch: true
+			} 
+		});
+	}
+// end of document.ready
+});
+// end of document.ready
---
+++ src/js/lib.js
@@ -0,0 +1,4 @@
+//=include lib/jquery-3.1.1.min.js
+//=include lib/jquery.carouFredSel-6.2.1-packed.js
+
+
